c01f8c8acea89b62fa1eca141b308a0eb249c4e0
Rishab <rishab@krishworks.com>
2023-10-31 19:39:26 +0530

    push to deploy

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,8 @@
 // export const API = "/api/v1";
 
 // export const API = "http://localhost:4000/api/v1";
-export const API = "http://192.168.1.10:4000/api/v1";
+// export const API = "http://192.168.1.10:4000/api/v1";
+export const API = "https://edtech-backend-08jx.onrender.com/api/v1";
+
 export const FRONTEND_DOMAIN = "http://localhost:3000"
+// export const FRONTEND_DOMAIN = "https://edtech-backend-08jx.onrender.com";
