89a30074624dbdd836817f5e0a794d729462488f
rishab singh <rishab.w.singh1234@gmail.com>
2024-01-26 16:01:49 +0530

    backend url changes by env

--- src/constant.js
+++ src/constant.js
@@ -3,7 +3,7 @@
 
 let API = "http://172.20.10.3:4000/api/v1";
 let FRONTEND_DOMAIN = "https://ed-frontend-kohl.vercel.app";
-
+console.log(process.env.NODE_ENV)
 if(process.env.NODE_ENV === "production"){
     FRONTEND_DOMAIN = window.location.protocol + "//" + window.location.host;
     API = "https://edtech-backend-08jx.onrender.com/api/v1";
